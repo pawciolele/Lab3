188f1c81190a5b4660f471f195500e8544678a59
pawciolele <157295752+pawciolele@users.noreply.github.com>
2024-07-13 01:05:01 +0200

    Create kom.sh

---
+++ kom.sh
@@ -0,0 +1,9 @@
+gitconfig --global user.email "pawcioleleeo@gmail.com"
+git config --global user.name "Pawciolele"
+git init
+git add lab3.sh
+git commit -m "Pierwszy commit - dodanie pliku lab3.sh"
+git push --set-upstream https://github.com/pawciolele/Lab3 master pawciolelee@gmail.com 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
+git add lab3.sh
+git commit -m "Tym razem zmieniono to i to..."
+git push
